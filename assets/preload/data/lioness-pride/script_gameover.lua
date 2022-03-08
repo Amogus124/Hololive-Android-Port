8234632267072e9b35744586783ad5b430ec2a53
Amogus124 <95672691+Amogus124@users.noreply.github.com>
2022-03-08 04:20:01 -0300

    Delete script_gameover.lua

--- assets/preload/data/lioness-pride/script_gameover.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'pico-player'); --Character json file for the death animation
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx_botan'); --put in mods/sounds/
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver_Botan'); --put in mods/music/
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd_Botan'); --put in mods/music/
-end
