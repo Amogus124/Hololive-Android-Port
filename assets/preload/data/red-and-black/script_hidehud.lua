abd692251c73982e9a7d36c4f67a76c6bbe21372
Amogus124 <95672691+Amogus124@users.noreply.github.com>
2022-03-08 04:25:11 -0300

    Delete the file at 'assets/preload/data/red-and-black/script_hidehud.lua'

--- assets/preload/data/red-and-black/script_hidehud.lua
+++
@@ -1,5 +0,0 @@
-function onStepHit()
-        if curStep ==1333 then --The number is when you want the thing to happen, you can view the curSteps in the chart editer
-        setProperty('camHUD.visible', false); --set the thing to false if you wanna hide the hud ,and true for it to appear   
-        end
-    end
